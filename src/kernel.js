c96c9ddfd0f2729521c5e5199b38f731f3b296f3
Bridge325-Git <brandon.sampson92@gmail.com>
2025-08-03 16:59:23 -0400

    Update kernel.js

--- src/kernel.js
+++ src/kernel.js
@@ -6,7 +6,7 @@ let userList = [];
 let mailList = [];
 let cmdLine_;
 let output_;
-let serverDate = { day: "", month: "", year: "", reference: "" };
+let serverDate = { day: "05", month: "01", year: "1996", reference: "" };
 
 function initDateObject() {
     const date = new Date();
